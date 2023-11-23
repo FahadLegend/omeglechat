face73481274e56ab95de8000329aec6a1b371f3
FahadLegend <150781148+FahadLegend@users.noreply.github.com>
2023-11-23 04:15:21 -0600

    Update connection.js

--- Server/database/connection.js
+++ Server/database/connection.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     //mongodb connection string
-    const con = await mongoose.connect(process.env.MONGO_URI);
+   // const con = await mongoose.connect(process.env.MONGO_URI);
+    const con = await mongoose.connect("mongodb+srv://alfarism993:Zainab1981@ome.xqqio07.mongodb.net/?retryWrites=true&w=majority");
     console.log(`MongoDB connected: ${con.connection.host}`);
   } catch (err) {
     console.log(err);
